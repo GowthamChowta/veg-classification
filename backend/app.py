368222284db19a83c29c24bb3fe1661c79ca8122
GowthamChowta <gautigadu091@gmail.com>
2022-12-08 04:08:10 -0500

    test

--- backend/app.py
+++ backend/app.py
@@ -64,9 +64,12 @@ def predict():
         print("Image loaded")
         img = np.expand_dims(img,0)
         y_pred = model.predict(img)
+        print(y_pred[0],y_pred[0].shape)
         top3PredictedIndexes = np.argsort(y_pred[0])[::-1][:3]
         top3PredictedClasses = CLASS_NAMES[top3PredictedIndexes]
+        print(top3PredictedClasses)
         top3PredictedPropbabilities = y_pred[top3PredictedIndexes]
+        print(top3PredictedPropbabilities)
                 
         
         return {"CLASS_NAMES":CLASS_NAMES[np.argmax(y_pred)], "classes": top3PredictedClasses, "prob": top3PredictedPropbabilities}
